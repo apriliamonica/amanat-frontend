73a3f2d1b13dda7c62cee480ba644dc691a3aa23
April <liapandeiroth34190@gmail.com.com>
2025-11-24 15:33:42 +0800

    Update validators.js (Tambah validateEmail)

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -28,3 +28,23 @@ export const validateFormData = (data, requiredFields) => {
     errors
   }
 }
+export const validateRequired = (value) => {
+  return value && value.trim().length > 0;
+};
+
+export const validateMailNumber = (mailNumber) => {
+  // Format: XXX/XXX/XXX/XXXX atau B/123/DIR/2024
+  const pattern = /^[A-Z0-9\/-]+$/i;
+  return pattern.test(mailNumber);
+};
+
+export const validateEmail = (email) => {
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return pattern.test(email);
+};
+
+export const validatePhone = (phone) => {
+  // Format Indonesia: 08xx-xxxx-xxxx atau +62xxx
+  const pattern = /^(\+62|08)[0-9]{8,12}$/;
+  return pattern.test(phone.replace(/[\s-]/g, ''));
+};
